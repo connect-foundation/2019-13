d22d390a6d6f7049e7dfb8762c132dd8291c51dc
munsulim <lms1427@gmail.com>
2019-11-10 23:40:12 +0900

    기타: 프론트 초기 환경 구축
    create-react-app을 통해 프로젝트를 생성하고 eslint, apollo boost, style 등을 설정함.

---
+++ front/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../Styles/GlobalStyles';
+import Theme from '../Styles/Theme';
+import Router from './Router';
+
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <>
+      <GlobalStyles />
+      <Router isLoggedIn="true" />
+    </>
+  </ThemeProvider>
+);
---
+++ front/src/Routes/Main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const main = () => <>This is logout main</>;
+
+export default main;
