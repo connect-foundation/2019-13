237a8574bb34af5f47e34d12e4857fba0fef6dc7
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-11-14 14:37:04 +0900

    추가: Prisma 구축 및 로그인 구현
    Prisma 구축하고
    Front에서 넘어온 accessToken을 이용하여 유저 여부 판단 후 유저DB에
    추가/수정하고 새로운 토큰을 발급하여 이를 이용해서 권한을 부여하는
    방식을 구현

--- back/src/passport/index.js
+++ back/src/passport/index.js
@@ -1,4 +1,6 @@
 import { Strategy as GoogleTokenStrategy } from "passport-google-token";
+import {prisma} from '../../prisma-client';
+import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt';
 export default passport => {
   passport.serializeUser((user, done) => {
     done(null, user);
@@ -6,16 +8,55 @@ export default passport => {
   passport.deserializeUser((user, done) => {
     done(null, user);
   });
-
+  
   passport.use(
     new GoogleTokenStrategy(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
       },
-      (accessToken, refreshToken, profile, done) => {
-        done(null, profile);
+      async (accessToken, refreshToken, profile, done) => {
+        //인증되었을 경우
+        const {id,email,name,picture} = profile._json;
+        try{
+          const user = await prisma.upsertUser({
+            where : {
+              id : `G-${id}`
+            },
+            create:{
+              id : `G-${id}`,
+              email,
+              name,
+              picture
+            },
+            update:{
+              email,
+              name,
+              picture
+            }
+          })
+       
+          done(null, user);
+        }catch(e){
+          done(e);
+        }
+       
       }
     )
   );
+  passport.use(new JwtStrategy({
+    jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey : process.env.JWT_SECRET
+  },async (jwt_payload, done)=>{
+    try{
+      const user = await prisma.$exists.user({
+        where : jwt_payload.id
+      });
+      if(user) return done({...user});
+      else return done(null, false);
+    }catch(e){
+      return(e);
+    }
+
+  }))
 };
