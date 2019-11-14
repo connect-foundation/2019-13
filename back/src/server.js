c2457eaaef19ec7d9ab77f69106e2cd7cb6f6b90
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-11-14 15:35:46 +0900

    추가: Prisma 구축 및 로그인 구현
    Prisma 구축하고
    Front에서 넘어온 accessToken을 이용하여 유저 여부 판단 후 유저DB에
    추가/수정하고 새로운 토큰을 발급하여 이를 이용해서 권한을 부여하는
    방식을 구현

--- back/src/server.js
+++ back/src/server.js
@@ -20,6 +20,12 @@ server.express.use(express.json());
 server.express.use(cors(corsOptions))
 server.express.use(logger('dev'));
 server.express.use(passport.initialize());
+server.express.use((req,res,next)=>{
+    if(!req.headers.authorization) return next(); 
+    passport.authenticate('jwt',{session:false},(err,user,info)=>{
+      next();
+    })(req,res,next);
+});
 passportConfig(passport);
 server.express.use('/auth',authRouter);
 
