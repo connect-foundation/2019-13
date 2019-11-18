fe06fcf65eee479d3b4263b9dfd558a45bac978a
NBJ1995 <darkmagic112@naver.com>
2019-11-18 12:58:25 +0900

    형식: eslint 적용 및 리뷰 받은 내용 수정 리뷰 = object 선언 시 x, y 프로퍼티를 선언할 것

--- front/src/Components/Block/Logic/Drag.js
+++ front/src/Components/Block/Logic/Drag.js
@@ -1,38 +1,39 @@
-const mouseHandler = (set) =>{
-  const mousedown = (e) => {
-    if(e.target.tagName !== 'path')
-      return
-    e.preventDefault()
+const mouseHandler = (set) => {
+  const mousedown = (eventDown) => {
+    if (eventDown.target.tagName !== 'path') { return; }
+    eventDown.preventDefault();
 
-    let currentPoint = {}
-    currentPoint.x = e.target.getBoundingClientRect().x - e.target.ownerSVGElement.getBoundingClientRect().x
-    currentPoint.y = e.target.getBoundingClientRect().y - e.target.ownerSVGElement.getBoundingClientRect().y
+    const currentPoint = { x: 0, y: 0 };
+    currentPoint.x = eventDown.target.getBoundingClientRect().x
+     - eventDown.target.ownerSVGElement.getBoundingClientRect().x;
+    currentPoint.y = eventDown.target.getBoundingClientRect().y
+     - eventDown.target.ownerSVGElement.getBoundingClientRect().y;
 
     set({
       x: currentPoint.x,
-      y: currentPoint.y
+      y: currentPoint.y,
     });
 
-    let startPoint = {}
-    startPoint.x = e.clientX - currentPoint.x
-    startPoint.y = e.clientY - currentPoint.y
+    const startPoint = { x: 0, y: 0 };
+    startPoint.x = eventDown.clientX - currentPoint.x;
+    startPoint.y = eventDown.clientY - currentPoint.y;
 
-    const mousemove = (e) => {
-      e.preventDefault()
+    const mousemove = (eventMove) => {
+      eventMove.preventDefault();
       set({
-        x: e.clientX - startPoint.x,
-        y: e.clientY - startPoint.y
+        x: eventMove.clientX - startPoint.x,
+        y: eventMove.clientY - startPoint.y,
       });
     };
 
-    const mouseup = (e) => {
-      document.removeEventListener("mousemove", mousemove);
-      document.removeEventListener("mouseup", mouseup);
+    const mouseup = () => {
+      document.removeEventListener('mousemove', mousemove);
+      document.removeEventListener('mouseup', mouseup);
     };
 
-    document.addEventListener("mousemove", mousemove);
-    document.addEventListener("mouseup", mouseup);
-  }
-  return mousedown
-}
-export default mouseHandler
+    document.addEventListener('mousemove', mousemove);
+    document.addEventListener('mouseup', mouseup);
+  };
+  return mousedown;
+};
+export default mouseHandler;
--- front/src/Components/Block/index.js
+++ front/src/Components/Block/index.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react'
-import Block from './block'
-import drag from './Logic/Drag'
+import React, { useState } from 'react';
+import Block from './block';
+import drag from './Logic/Drag';
 import motion from './Init/Motion';
 
 export default () => {
-  const blocks = []
+  const blocks = [];
   motion.forEach((json) => {
     const block = new Block();
-    block.makeFromJSON(json)
+    block.makeFromJSON(json);
 
-    const [position, setPosition] = useState({ x: 0, y: 0 })
-    const g = React.createElement("g", {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const g = React.createElement('g', {
       onMouseDown: drag(setPosition),
-      transform: `translate(${position.x},${position.y})`
+      transform: `translate(${position.x},${position.y})`,
     }, block.path, ...block.args);
 
-    blocks.push(g)
-  })
+    blocks.push(g);
+  });
 
   return (
     <svg>
-      {blocks.map((block) => { return block })}
+      {blocks.map(block => block)}
     </svg>
-  )
-}
+  );
+};
