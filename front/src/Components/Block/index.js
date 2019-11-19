76b11f644d1f0f080497b9d2742d802f515cd5ce
munsulim <lms1427@gmail.com>
2019-11-19 13:05:38 +0900

    리팩토링: 초기에 한번 생성한 블록으로 렌더링하도록 변경.
    workspace 객체를 생성하고 해당 객체의 blockDB에 추가한 블록으로 렌더링되도록 수정함.

--- front/src/Components/Block/index.js
+++ front/src/Components/Block/index.js
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
-import Block from './block';
+import styled from 'styled-components';
 import drag from './Logic/Drag';
 import motion from './Init/Motion';
+import Workspace from './workspace';
 
 export default () => {
+  const [isInit, setIsInit] = useState(false);
+  const [workspace] = useState(new Workspace());
   const blocks = [];
-  motion.forEach((json) => {
-    const block = new Block();
-    block.makeFromJSON(json);
 
+  if (!isInit) {
+    setIsInit(true);
+    motion.forEach((json) => {
+      const block = workspace.addBlock();
+      block.makeFromJSON(json);
+    });
+  }
+
+  Object.values(workspace.blockDB).forEach((block) => {
     const [position, setPosition] = useState({ x: 0, y: 0 });
-    const g = React.createElement('g', {
-      onMouseDown: drag(setPosition),
-      transform: `translate(${position.x},${position.y})`,
-    }, block.path, ...block.args);
-
+    const g = React.createElement(
+      'g',
+      {
+        onMouseDown: drag(setPosition),
+        transform: `translate(${position.x},${position.y})`,
+        key: block.id,
+      },
+      block.path,
+      ...block.args,
+    );
     blocks.push(g);
   });
 
   return (
-    <svg>
+    <Svg>
       {blocks.map(block => block)}
-    </svg>
+    </Svg>
   );
 };
+
+const Svg = styled.svg`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
