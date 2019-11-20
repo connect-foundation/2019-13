b20668f56518b7a8bdd585ef24c7f26782c3dc90
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-11-20 15:10:57 +0900

    형식: modelspace => blockModelList 변경
    변수명 동일하게 사용하기 위해서 변경

--- front/src/Components/Block/index.js
+++ front/src/Components/Block/index.js
@@ -6,7 +6,7 @@ import Group from '../Group';
 import BlockModelList from './block_model_list';
 
 const position = { x: 75, y: 25 };
-const modelspace = new BlockModelList();
+const blockModelList = new BlockModelList();
 export default () => {
   const [isInit, setIsInit] = useState(false);
   const { workspace } = useContext(WorkspaceContext);
@@ -14,14 +14,14 @@ export default () => {
   if (!isInit) {
     setIsInit(true);
     motion.forEach((json, idx) => {
-      const block = modelspace.addBlock();
+      const block = blockModelList.addBlock();
       block.makeFromJSON({ ...json, x: position.x, y: (position.y + 75 * idx), motionIndex: idx });
     });
   }
 
   return (
     <Svg>
-      {Object.values(modelspace.blockDB).map(block => (
+      {Object.values(blockModelList.blockDB).map(block => (
         <Group
           block={block}
         />
