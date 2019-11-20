03310988d236f74973c197df1aa658b9d26f0c7e
NBJ1995 <darkmagic112@naver.com>
2019-11-20 16:28:00 +0900

    기타: React Element가 DOM에 생성 된 후 DOM 객체를 전달 Block 객체에서 접근할 수 있게끔 객체를 전달함

--- front/src/Components/Group/index.js
+++ front/src/Components/Group/index.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropType from 'prop-types';
 import drag from '../Block/Logic/Drag';
 
 
 const Group = ({ block }) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  console.log(block);
+  const [, setRender] = useState();
+  const gRef = useRef();
+  useEffect(() => {
+    block.render = setRender;
+    block.setNode(gRef.current);
+  }, []);
   return (
     <g
+      key={block.id}
+      ref={gRef}
       onMouseDown={drag(setPosition)}
       transform={`translate(${position.x},${position.y})`}
     >
