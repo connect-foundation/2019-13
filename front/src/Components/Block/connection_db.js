e9b3e9b72fefbce0fe901f865f395e085d72f24b
munsulim <lms1427@gmail.com>
2019-11-20 14:54:27 +0900

    기타: connection에 따른 변경 사항 및 예외처리
    connection에서 변경된 방법으로 y값 비교하도록 설정하고 findStartIdxForConnection의
    예외처리 코드 추가.

--- front/src/Components/Block/connection_db.js
+++ front/src/Components/Block/connection_db.js
@@ -7,79 +7,98 @@
  * boostcamp the duck
  *
  *
- **/
+ * */
 
-const ConnectionDB = function() {
+const ConnectionDB = function () {
   this.connections = [];
 };
 
-ConnectionDB.prototype.addConnection = function(connection) {
+ConnectionDB.prototype.reset = function () {
+  this.connections = [];
+};
+
+ConnectionDB.prototype.setConnections = function (block) {
+  Object.values(block.workspace.blockDB).forEach((b) => {
+    if (!b.isDragged) {
+      this.connections.push(...b.getAvailableConnection());
+    }
+  });
+  this.sortAxisY();
+};
+
+ConnectionDB.prototype.addConnection = function (connection) {
   this.connections.push(connection);
 };
 
-ConnectionDB.prototype.isInYRange = function(connectIdx, y, maxRadius) {
-  return Math.abs(this.connections[connectIdx].test_y - y) <= maxRadius;
+ConnectionDB.prototype.isInYRange = function (connectIdx, y, maxRadius) {
+  return Math.abs(this.connections[connectIdx].Y_() - y) < maxRadius;
 };
 
-ConnectionDB.prototype.findClosetConnection = function(connect, maxRadius) {
-  const startIdx = this.findStartIdxForConnection(connect.X_(), connect.Y_());
+ConnectionDB.prototype.findClosetConnection = function (connect, maxRadius) {
+  const startIdx = this.findStartIdxForConnection(connect.Y_());
   let closetConnection = null;
   let bestRadius = maxRadius;
   let temp;
   let findBackword = startIdx - 1;
+  let findForward = startIdx;
+
   if (startIdx >= this.connections.length) {
     return -1;
   }
   while (
-    findBackword >= 0 &&
-    this.isInYRange(findBackword, connect.Y_(e), bestRadius)
+    findBackword >= 0
+    && this.isInYRange(findBackword, connect.Y_(), bestRadius)
   ) {
     temp = this.connections[findBackword];
     if (connect.canConnect(temp, bestRadius)) {
       closetConnection = temp;
       bestRadius = temp.distanceFrom(connect);
     }
-    findBackword--;
+    findBackword -= 1;
   }
-  let findForward = startIdx;
+
   while (
-    findForward < this.connections.length &&
-    this.isInYRange(findForward, connect.Y_(e), bestRadius)
+    findForward < this.connections.length
+    && this.isInYRange(findForward, connect.Y_(), bestRadius)
   ) {
     temp = this.connections[findForward];
     if (connect.canConnect(temp, bestRadius)) {
       closetConnection = temp;
       bestRadius = temp.distanceFrom(connect);
     }
-    findForward++;
+    findForward += 1;
   }
+
   return { connection: closetConnection, radius: bestRadius };
 };
 
-ConnectionDB.prototype.findStartIdxForConnection = function(test_x, test_y) {
+ConnectionDB.prototype.findStartIdxForConnection = function (Y) {
+  let positionMin = 0;
+  let positionMax = this.connections.length;
+
   if (!this.connections.length) {
     return 0;
   }
-  let positionMin = 0;
-  let positionMax = this.connections.length;
+
   while (positionMin < positionMax) {
-    let postionMid = Math.floor((positionMin + positionMax) / 2);
-    if (this.connections[postionMid].test_y < test_y) {
+    const postionMid = Math.floor((positionMin + positionMax) / 2);
+    if (this.connections[postionMid].Y_() < Y) {
       positionMin = postionMid + 1;
-    } else if (this.connections[postionMid].test_y > test_y) {
+    } else if (this.connections[postionMid].Y_() > Y) {
       positionMax = postionMid;
     } else {
       positionMin = postionMid;
       break;
     }
   }
+  if (positionMin >= this.connections.length) {
+    positionMin = this.connections.length - 1;
+  }
   return positionMin;
 };
 
-ConnectionDB.prototype.sortAxisY = function() {
-  this.connections.sort(function(a, b) {
-    return a.Y_() - b.Y_();
-  });
+ConnectionDB.prototype.sortAxisY = function () {
+  this.connections.sort((a, b) => a.Y_() - b.Y_());
 };
 
 export default ConnectionDB;
