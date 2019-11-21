616c9fc65e428c52bad98bf96501e3a181601451
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-11-21 13:51:04 +0900

    추가: facebook OAuth 로그인 구현 (Front-end)

--- front/src/Components/Auth/index.js
+++ front/src/Components/Auth/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-alert */
 import React, { useContext } from 'react';
 import { GoogleLogin } from 'react-google-login';
+import FaceBookLogin from 'react-facebook-login';
 import sendRequest from '../../utils/SendRequest';
 import { LoggedInContext, ModalContext } from '../../Context';
 import { setLocalStorageItem } from '../../utils/storage';
@@ -22,7 +23,34 @@ export default () => {
       alert('로그인이 되지 않았습니다. 다시 로그인해주세요');
       return;
     }
-    setLocalStorageItem([{ key: 'token', value: data.token }, { key: 'userImage', value: response.profileObj.imageUrl }]);
+    setLocalStorageItem([
+      { key: 'token', value: data.token },
+      { key: 'userImage', value: response.profileObj.imageUrl },
+    ]);
+    setLoggedIn(data.result);
+    setOpen(!data.result);
+  };
+  const responseFacebook = async (response) => {
+    const body = JSON.stringify({
+      access_token: response.accessToken,
+    });
+    console.log(response);
+    const data = await sendRequest({
+      url: process.env.REACT_APP_AUTH_FACEBOOK,
+      method: 'post',
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!data.result) {
+      alert('로그인이 되지 않았습니다. 다시 로그인해주세요');
+      return;
+    }
+    setLocalStorageItem([
+      { key: 'token', value: data.token },
+      { key: 'userImages', value: response.picture.data.url },
+    ]);
     setLoggedIn(data.result);
     setOpen(!data.result);
   };
@@ -35,7 +63,12 @@ export default () => {
         // onFailure={responseGoogle}
         cookiePolicy="single_host_origin"
       />
-
+      <FaceBookLogin
+        appId={process.env.REACT_APP_FACEBOOK_CLIENT_ID}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        icon="fa-facebook"
+      />
     </>
   );
 };
