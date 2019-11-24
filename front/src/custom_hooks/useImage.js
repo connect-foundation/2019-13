7989b305c7d2ac26e9e15b647df7d442b8bd2837
KIM <kkw01234@gmail.com>
2019-11-24 19:39:31 +0900

    추가: useImage라는 CustomHooks 생성
    이미지가 src로 들어간 후 load될떄까지 기다리는 것을 CustomHooks를
    이용하여 구현

---
+++ front/src/custom_hooks/useImage.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+// const defaultState = {
+//   image: undefined,
+//   x: 0,
+//   y: 0,
+//   size: 1,
+//   direction: 0,
+// };
+
+export default ({ url, x, y, size = 100, direction = 0 }) => {
+  const [image, setImage] = useState(undefined);
+
+  useEffect(() => {
+    if (!url) return;
+    const img = new Image();
+    img.src = url;
+
+
+    const onLoad = () => {
+      img.width = img.width ? img.width * (size / 100) : 100 * (size / 100);
+      img.height = img.height ? img.height * (size / 100) : 100 * (size / 100);
+      setImage(img);
+    };
+    img.addEventListener('load', onLoad);
+  }, [url, x, y, size, direction]);
+
+  return [image, setImage];
+};
