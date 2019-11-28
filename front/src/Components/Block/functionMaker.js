50d0b427c41b7d23b732c60e197598fc0fc7c846
munsulim <lms1427@gmail.com>
2019-11-28 14:32:13 +0900

    추가: fucntionMaker에 탑블록 및 실행 로직  추가
    탑블록을 한번 돌아가는 포문으로 감싸고 포문에서 내부 블록의 실행 순서에 맞게 실행 될 수 있
    도록 로직을 변경하였음. (ex: for문 내부에 for가 있는 경우 내부 포문이 끝나고 다음 블록을 실
    행시키고 그것을 반복하도록)

--- front/src/Components/Block/functionMaker.js
+++ front/src/Components/Block/functionMaker.js
@@ -1,35 +1,104 @@
-export default {
+import Move from '../DrawSection/function/move';
+import Rotate from '../DrawSection/function/rotate';
+
+const FunctionList = {
+  topblock: (args) => {
+    const codes = args;
+    let isChildEnd = false;
+    let i = 0;
+    let j = 0;
+    function* ONCE() {
+      while (i < 1) {
+        while (j < codes.length) {
+          codes[j].isPlay = true;
+          isChildEnd = true;
+          const cur = codes[j].func.next();
+          if (cur && cur.value < codes[j].limit) {
+            isChildEnd = false;
+            break;
+          }
+          j += 1;
+        }
+        yield i += isChildEnd ? 1 : 0;
+      }
+    }
+    return { func: ONCE() };
+  },
   control_for: (args) => {
     const limit = args.input;
+    let isChildEnd = false;
+    let i = 0;
+    let j = 0;
     const codes = args.firstChild;
-    return () => {
-      for (let i = 0; i < limit; i += 1) {
-        codes.forEach(func => func());
+    function* FOR_LOOP() {
+      while (true) {
+        if (i >= limit) i = 0;
+        if (j === codes.length) j = 0;
+        while (j < codes.length) {
+          codes[j].func.isPlay = true;
+          isChildEnd = true;
+          const cur = codes[j].func.next();
+          if (cur && cur.value < codes[j].limit) {
+            isChildEnd = false;
+            break;
+          }
+          j += 1;
+        }
+        yield i += isChildEnd ? 1 : 0;
       }
-    };
+    }
+    return { func: FOR_LOOP(), limit };
   },
 
   motion_move_forward: (args) => {
     const value = args.input;
-    return () => {
-      // canvas 조작 함수가 들어와야함.
-      // ex move(value)
+    const func = { isPlay: true };
+    func.next = () => {
+      if (func.isPlay) Move({ moving: value });
+      func.isPlay = false;
     };
+    return { func };
   },
 
   motion_change_x: (args) => {
     const value = args.input;
-    return () => {
-      // canvas 조작 함수가 들어와야함.
-      // ex move(value, 'x')
+    const func = { isPlay: true };
+    func.next = () => {
+      if (func.isPlay) Move({ x: value }, 'x');
+      func.isPlay = false;
     };
+    return { func };
   },
 
   motion_change_y: (args) => {
     const value = args.input;
-    return () => {
-      // canvas 조작 함수가 들어와야함.
-      // ex move(value, 'y')
+    const func = { isPlay: true };
+    func.next = () => {
+      if (func.isPlay) Move({ y: value }, 'y');
+      func.isPlay = false;
     };
+    return { func };
+  },
+
+  motion_rotate_clock: (args) => {
+    const value = args.input;
+    const func = { isPlay: true };
+    func.next = () => {
+      if (func.isPlay) Rotate(value, 'clock');
+      func.isPlay = false;
+    };
+    return { func };
+  },
+
+  motion_rotate_anti_clock: (args) => {
+    const value = args.input;
+    const func = { isPlay: true };
+    func.next = () => {
+      if (func.isPlay) Rotate(value, 'anticlock');
+      func.isPlay = false;
+    };
+    return { func };
   },
 };
+
+export default FunctionList;
