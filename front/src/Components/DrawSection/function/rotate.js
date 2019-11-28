f50f51f7f68dace83dab0f628a996e3644dd177e
KIM <kkw01234@gmail.com>
2019-11-28 10:25:07 +0900

    변경: rotate 함수
    그 방향으로 가리키는 것을 추가하기위해서 변경

--- front/src/Components/DrawSection/function/rotate.js
+++ front/src/Components/DrawSection/function/rotate.js
@@ -2,13 +2,30 @@ import { getPosition } from '../index';
 import Utils from '../../../utils/utils';
 /**
  * @param {Number} movement 회전반경(0~360)
- * @param {Boolean} type 시계방향(true), 반시계방향(false)
+ * @param {String} type 'clock' => 시계방향으로 movement만큼 회전, 'anticlock' 반시계방향으로 movement만큼 회전,  designation => movement각도로 지정
  *
  */
 export default (movement, type) => {
   const { key, position, dispatch } = getPosition();
-  const value = {
-    direction: Utils.parseInt10(position.direction) + (type ? movement : -movement),
-  };
+  let value;
+  switch (type) {
+    case 'clock':
+      value = {
+        direction: Utils.parseInt10(position.direction) + movement,
+      };
+      break;
+    case 'anticlock':
+      value = {
+        direction: Utils.parseInt10(position.direction) - movement,
+      };
+      break;
+    case 'designation':
+      value = {
+        direction: movement,
+      };
+      break;
+    default:
+      throw new TypeError('NOT FOUND TYPE');
+  }
   dispatch({ type: 'ROTATE', key, value });
 };
