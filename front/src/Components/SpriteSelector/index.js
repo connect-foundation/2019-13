81a7a1b766cc057ea5fe2f2c1836c1d272c17db3
KIM <kkw01234@gmail.com>
2019-12-04 23:19:18 +0900

    추가 : image selector 그림 추가

--- front/src/Components/SpriteSelector/index.js
+++ front/src/Components/SpriteSelector/index.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import DialMenu from '../DialMenu';
-import Sprite from '../Sprite';
+import SpriteSelectorItem from '../SpriteSelectorItem';
+import { SpritesContext } from '../../Context';
 
 export default () => {
-  const [test, setTest] = useState(null);
+  const { sprites } = useContext(SpritesContext);
+  console.log(sprites);
   return (
     <SpriteContainer>
-      <Sprite />
+      {Object.entries(sprites).map(sprite => <SpriteSelectorItem sprite={sprite[1]} key={sprite[0]} />)}
       <DialMenu />
     </SpriteContainer>
   );
 };
 
 const SpriteContainer = styled.div`
+  position : relative;
+  display : inline-flex;
+  flex-wrap: wrap;
   padding: 10px;
   border: 1px solid black;
   border-radius: 10px;
   width: 100%;
-  height: 400px;
+  height: 300px;
   background-color: #e9f1fc;
 `;
--- front/src/Components/SpriteSelectorItem/index.js
+++ front/src/Components/SpriteSelectorItem/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default () => (
+export default ({ sprite }) => (
   <SpriteContainer>
-    <Image />
-    <Title />
+    <Image src={sprite.url} />
+    <Title>{sprite.url}</Title>
   </SpriteContainer>
 );
 const Image = styled.img`
-    height : 70%;
-    
+  height: 70%;
+  transform:translate(25%, 0%);
 `;
 const Title = styled.div`
-    height : 30%;
-    background-color : white;
+  height: 30%;
+  background-color: white;
+  overflow:hidden;
+  text-overflow:ellipsis;
 `;
 
 const SpriteContainer = styled.div`
   width: 80px;
   height: 80px;
-  padding-bottom : 5px;
+  padding-bottom: 5px;
   border: 1px solid black;
   border-radius: 5px;
+  background-color:white;
 `;
