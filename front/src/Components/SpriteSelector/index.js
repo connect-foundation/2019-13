76a80442ff75e0dacdb08519c2b1113430ee5f13
KIM <kkw01234@gmail.com>
2019-12-05 17:46:02 +0900

    추가: 이미지 추가시 spriteselector에도 추가

--- front/src/Components/SpriteSelector/index.js
+++ front/src/Components/SpriteSelector/index.js
@@ -6,23 +6,25 @@ import { SpritesContext } from '../../Context';
 
 export default () => {
   const { sprites } = useContext(SpritesContext);
-  console.log(sprites);
   return (
     <SpriteContainer>
-      {Object.entries(sprites).map(sprite => <SpriteSelectorItem sprite={sprite[1]} key={sprite[0]} />)}
+      {Object.entries(sprites).map(sprite => (
+        <SpriteSelectorItem sprite={sprite[1]} key={sprite[0]} spritekey={sprite[0]} />
+      ))}
       <DialMenu />
     </SpriteContainer>
   );
 };
 
 const SpriteContainer = styled.div`
-  position : relative;
-  display : inline-flex;
+  position: relative;
+  display: flex;
+  align-items: flex-start;
   flex-wrap: wrap;
   padding: 10px;
   border: 1px solid black;
   border-radius: 10px;
-  width: 100%;
-  height: 300px;
+  max-width: 100%;
+  min-height: 300px;
   background-color: #e9f1fc;
 `;
--- front/src/Components/SpriteSelectorItem/index.js
+++ front/src/Components/SpriteSelectorItem/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
+import { CurrentSpriteContext } from '../../Context';
 
-export default ({ sprite }) => (
-  <SpriteContainer>
-    <Image src={sprite.url} />
-    <Title>{sprite.url}</Title>
-  </SpriteContainer>
-);
+export default ({ sprite, spritekey }) => {
+  const { currentSprite, setCurrentSprite } = useContext(CurrentSpriteContext);
+  const [isclicked, setClicked] = useState(false);
+  const onClickHandler = () => {
+    setCurrentSprite({ key: spritekey, position: sprite });
+  };
+  return (
+    <SpriteContainer onClick={onClickHandler}>
+      <ImageWrapper>
+        <Image src={sprite.url} />
+      </ImageWrapper>
+      <Title>{sprite.url}</Title>
+    </SpriteContainer>
+  );
+};
 const Image = styled.img`
-  height: 70%;
-  transform:translate(25%, 0%);
+  max-width : 100%;
+  max-height : 100%;
 `;
 const Title = styled.div`
   height: 30%;
@@ -18,11 +28,24 @@ const Title = styled.div`
   text-overflow:ellipsis;
 `;
 
+const ImageWrapper = styled.div`
+  display : inline-block;
+  /* vertical-align : center; */
+  text-align:center;
+  max-height : 70%;
+`;
+
 const SpriteContainer = styled.div`
   width: 80px;
   height: 80px;
+  margin-right: 10px;
   padding-bottom: 5px;
-  border: 1px solid black;
+  border:  1px solid ${props => props.color || 'black'};
   border-radius: 5px;
   background-color:white;
+  display : flex;
+  flex-direction : column;
+  justify-content: space-between;
+  
+
 `;
