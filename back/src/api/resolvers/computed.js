7c2ee7da936a618346aac0567a9981951819c48e
munsulim <lms1427@gmail.com>
2019-12-09 00:23:48 +0900

    추가: 백엔드 좋아요 기능 추가.
    백엔드에 좋아요 기능을 togglelike로 구현하여 좋아요를 이미 했다면 취소, 안했다면 좋
    아요를 생성하도록 구현함.

---
+++ back/src/api/resolvers/computed.js
@@ -0,0 +1,32 @@
+import { prisma } from '../../../prisma-client';
+import utils from '../../utils/utils';
+
+export default {
+  project: {
+    owner: ({ id }) => prisma.project({ id }).owner(),
+    blocks: ({ id }) => prisma.project({ id }).blocks(),
+    isLiked: async (root, value, context) => {
+      const { id } = root;
+      const user = utils.findUser(context.req);
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            project: {
+              id,
+            },
+          },
+        ],
+      });
+    },
+    likeCount: (parent) => prisma.likesConnection({
+      where: { project: { id: parent.id } },
+    })
+      .aggregate()
+      .count(),
+  },
+};
