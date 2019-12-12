e97a468d4847af9a2cf65fd364feeba7f0f32711
munsulim <lms1427@gmail.com>
2019-12-12 17:52:20 +0900

    추가: 댓글을 추가하고 자신의 댓글을 수정, 삭제하는 기능 추가.
    로그인된 사용자가 댓글을 추가하고 자신의 댓글을 수정, 삭제할 수 있는 기능을 추가하였습니다.

---
+++ front/src/Components/Comment/Comments.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
+import Comment from './Comment';
+import { LOAD_COMMENT } from '../../Apollo/queries/Comment';
+import CommentWriter from './CommentWriter';
+
+export default ({ project, user }) => {
+  const [commentCount, setCommentCount] = useState(0);
+  const [comments, setComments] = useState();
+  const [readComment] = useLazyQuery(LOAD_COMMENT, {
+    onCompleted(res) {
+      setCommentCount(res.findProjectById.commentCount);
+      setComments(res.findProjectById.comments);
+    },
+  });
+
+  useEffect(() => {
+    readComment({ variables: { projectId: project.id } });
+  }, []);
+
+  const localUpdate = (idx, data) => {
+    if (data) comments.splice(idx, 1, data);
+    else {
+      comments.splice(idx, 1);
+      setCommentCount(commentCount - 1);
+    }
+    setComments([...comments]);
+  };
+  if (!comments) return <></>;
+
+  const updateComments = (text) => {
+    readComment({ variables: { projectId: project.id } });
+  };
+
+  return (
+    <CommentWrapper user={user}>
+      <div>{`댓글 ${commentCount} 개`}</div>
+      {(user) ? (<CommentWriter projectId={project.id} updateComments={updateComments} />) : (<></>) }
+      {comments.map((comment, i) => (<Comment comment={comment} user={user} key={comment.id} idx={i} localUpdate={localUpdate} />))}
+    </CommentWrapper>
+  );
+};
+
+const CommentWrapper = styled.div`
+  margin-top: 50px;
+  word-wrap: break-word;
+  #commentWriter {
+    display: flex;
+    margin-top: 20px;
+    justify-content: space-between;
+  }
+  textarea {
+    resize: none;
+    width: 90%;
+    height: 100px;
+    font-size: 16px;
+    padding: 10px;
+  }
+  button {
+    margin-top: 10px;
+    margin-left: 90%;
+    width:10%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    font-weight: bold;
+    background: #5fa0ff;
+    color: white;
+    cursor: pointer;
+  }
+  #myImg {
+    margin: 0px 10px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-image: url(${props => (props.user ? props.user.picture : undefined)});
+    background-size: cover;
+  }
+`;
