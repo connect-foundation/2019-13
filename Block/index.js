dbc12250f4ade08911f82c7b496f00b2914eb1e5
NBJ1995 <darkmagic112@naver.com>
2019-12-13 11:53:15 +0900

    기타: 리뷰용 푸쉬

---
+++ Block/index.js
@@ -0,0 +1,191 @@
+import React, { useState, useContext } from 'react';
+import PropType from 'prop-types';
+import styled from 'styled-components';
+import { WorkspaceContext } from '../../Context';
+import Group from '../Group';
+import GroupBlock from '../GroupModel';
+import BlockModelList from './blockmodel_list';
+import CONSTANTS from './constants';
+import BlockModel from './blockmodel';
+import init from './Init';
+import Theme from '../../Styles/Theme';
+
+const blockModelList = new BlockModelList();
+const yArray = [];
+let previousClickedButton = 0;
+
+const Blocks = ({ clickedButton }) => {
+  const [isInit, setIsInit] = useState(false);
+  const { workspace, workspaceDispatch } = useContext(WorkspaceContext);
+  const [, setRender] = useState(0);
+  const [isMove, setMove] = useState(false);
+  const [scrollY, setScrollY] = useState(CONSTANTS.SCROLL_MINIMUM);
+  const [initY, setInitY] = useState(CONSTANTS.SCROLL_MINIMUM);
+  if (yArray.length > 0 && previousClickedButton !== clickedButton) {
+    if (scrollY !== yArray[parseInt(clickedButton, 10)]) {
+      previousClickedButton = clickedButton;
+      setScrollY(yArray[parseInt(clickedButton, 10)]);
+      workspaceDispatch({ type: 'SCROLL_END' });
+    }
+  }
+  const dragStartHandler = (scrollEvent) => {
+    setMove(true);
+    setInitY(scrollEvent.clientY - scrollEvent.target.getBoundingClientRect().y
+    + scrollEvent.target.ownerSVGElement.getBoundingClientRect().y);
+  };
+  const dragMoveHandler = (scrollEvent) => {
+    if (!isMove) return;
+    let diff = (scrollEvent.clientY - initY);
+    if (diff < CONSTANTS.SCROLL_MINIMUM)diff = CONSTANTS.SCROLL_MINIMUM;
+    else if (diff > CONSTANTS.SCROLL_MAXIMUM)diff = CONSTANTS.SCROLL_MAXIMUM;
+    workspaceDispatch({ type: 'SCROLL_END' });
+    setScrollY(diff);
+  };
+  const dragEndHandler = () => {
+    if (!isMove) return;
+    setMove(false);
+  };
+
+  const clickHandler = (scrollEvent) => {
+    let currentY = scrollEvent.clientY - scrollEvent.target.getBoundingClientRect().y;
+    if (currentY < CONSTANTS.SCROLL_MINIMUM)currentY = CONSTANTS.SCROLL_MINIMUM;
+    else if (currentY > CONSTANTS.SCROLL_MAXIMUM)currentY = CONSTANTS.SCROLL_MAXIMUM;
+    setScrollY(currentY);
+  };
+
+  const wheelSVG = (event) => {
+    if (event.clientX > CONSTANTS.DELETE_AREA_X + CONSTANTS.BUTTON_AREA_WIDTH
+      || event.clientX < CONSTANTS.BUTTON_AREA_WIDTH) return;
+    let newY = scrollY + event.deltaY;
+    if (newY < CONSTANTS.SCROLL_MINIMUM)newY = CONSTANTS.SCROLL_MINIMUM;
+    else if (newY > CONSTANTS.SCROLL_MAXIMUM)newY = CONSTANTS.SCROLL_MAXIMUM;
+    workspaceDispatch({ type: 'SCROLL_END' });
+    setScrollY(newY);
+  };
+
+  if (!isInit) {
+    setIsInit(true);
+    workspace.setRender = setRender;
+    let idx = 0;
+    let { y } = CONSTANTS.DEFAULT_POSITION;
+    init.forEach((blocks, allIdx) => {
+      yArray.push(y - 3);
+      blocks.forEach((json, styleIdx) => {
+        const blockModel = new BlockModel(idx).makeFromJSON({
+          ...json,
+          x: CONSTANTS.DEFAULT_POSITION.x,
+          y,
+          allIdx,
+          styleIdx,
+        });
+        idx += 1;
+        switch (json.style) {
+          case 'double':
+            y += 100;
+            break;
+          case 'variable':
+          case 'condition':
+            y += 35;
+            break;
+          default:
+            y += 50;
+        }
+        blockModelList.addBlock(blockModel);
+      });
+    });
+  }
+  return (
+    <Svg onWheel={wheelSVG} transform={`translate(${CONSTANTS.BUTTON_AREA_WIDTH},0)`}>
+      {isMove ? null
+        : (
+          <rect
+            width="300"
+            height="800"
+            fill="rgba(0,0,0,0)"
+            x="0"
+            y="0"
+            rx="4"
+            ry="4"
+            onMouseUp={dragEndHandler}
+            onMouseMove={dragMoveHandler}
+            onMouseLeave={dragEndHandler}
+            onClick={clickHandler}
+          />
+        )
+      }
+      {[...blockModelList.getBlockDB().values()].map(block => (
+        <GroupBlock block={block} key={block.id} scrollY={scrollY} />
+      ))}
+      {workspace.topblocks.map(block => <Group block={block} key={block.id} scroll={isMove} />)}
+      {!isMove ? null
+        : (
+          <rect
+            width="300"
+            height="800"
+            fill="rgba(0,0,0,0)"
+            x="0"
+            y="0"
+            rx="4"
+            ry="4"
+            onMouseUp={dragEndHandler}
+            onMouseMove={dragMoveHandler}
+            onMouseLeave={dragEndHandler}
+            onClick={clickHandler}
+          />
+        )}
+
+      <rect
+        width="20"
+        height="100"
+        fill={isMove ? Theme.duckOrangeColor : Theme.unactivedColor}
+        x="275"
+        y={scrollY}
+        rx="4"
+        ry="4"
+        onMouseDown={dragStartHandler}
+        onMouseUp={dragEndHandler}
+        onMouseMove={dragMoveHandler}
+      />
+    </Svg>
+  );
+};
+
+Blocks.propTypes = {
+  clickedButton: PropType.number.isRequired,
+};
+
+const Svg = styled.svg`
+  position: absolute;
+  width: 70%;
+  height: 84%;
+  g {
+    cursor: grab;
+    &:active {
+      cursor: grabbing;
+    }
+  }
+  text {
+    font-size: 12px;
+    fill: white;
+    pointer-events: none;
+    user-select: none;
+  }
+  foreignObject {
+    width: 30px;
+    height: 30px;
+    input {
+      width: 30px;
+      height: 20px;
+      border-radius: 18px;
+      text-align: center;
+      border: none;
+      padding: 0;
+      font-size: 8px;
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+`;
+
+export default Blocks;
---
+++ Routes/Project.js
@@ -0,0 +1,189 @@
+import React, { useReducer, useState } from 'react';
+import styled from 'styled-components';
+import Blockspace from '../Components/Block/index';
+import Workspace from '../Components/Block/workspace';
+import ProjectHeader from '../Components/projectHeader';
+import { WorkspaceContext, SpritesContext } from '../Context/index';
+import { workspaceReducer, spritesReducer } from '../reducer';
+import Utils from '../utils/utils';
+import DrawSection from '../Components/DrawSection';
+import Test from './test'
+
+const defaultSprite = {};
+defaultSprite[Utils.uid()] = {
+  name: 'logo.png',
+  url: '/logo.png',
+  size: 100,
+  direction: 90,
+  x: 0,
+  y: 0,
+  reversal: false,
+  realName: '/logo.png',
+};
+
+const Project = (props) => {
+  const [workspace, workspaceDispatch] = useReducer(
+    workspaceReducer,
+    new Workspace(),
+  );
+  const [isReady, setReady] = useState(false);
+  const [sprites, spritesDispatch] = useReducer(
+    spritesReducer,
+    defaultSprite,
+  );
+  const [clickedButton, setClickedButton] = useState(0);
+  return (
+    <WorkspaceContext.Provider value={{ workspace, workspaceDispatch }}>
+      <SpritesContext.Provider value={{ sprites, spritesDispatch }}>
+        <Wrapper>
+          <ProjectHeader props={props} setReady={setReady} />
+          <Contents>
+            <Blockspace clickedButton={clickedButton} />
+            <div className="Contents__Column block-types">
+              <TypesButton className="block-types__button" btype="motion" onClick={() => { setClickedButton(0 + Math.random()); }}>
+                <div />
+                <span> 동작 </span>
+              </TypesButton>
+              <TypesButton className="block-types__button" btype="events" onClick={() => { setClickedButton(1 + Math.random()); }}>
+                <div />
+                <span> 이벤트 </span>
+              </TypesButton>
+              <TypesButton className="block-types__button" btype="control" onClick={() => { setClickedButton(2 + Math.random()); }}>
+                <div />
+                <span> 제어 </span>
+              </TypesButton>
+              <TypesButton className="block-types__button" btype="operators" onClick={() => { setClickedButton(3 + Math.random()); }}>
+                <div />
+                <span> 연산 </span>
+              </TypesButton>
+              <TypesButton className="block-types__button" btype="sensing" onClick={() => { }}>
+                <div />
+                <span> 감지 </span>
+              </TypesButton>
+              <TypesButton className="block-types__button" btype="variables" onClick={() => { }}>
+                <div />
+                <span> 변수 </span>
+              </TypesButton>
+              <TypesButton className="block-types__button" btype="myblocks" onClick={() => { }}>
+                <div />
+                <span> 나의 블록 </span>
+              </TypesButton>
+            </div>
+            <div className="Contents__Column block-lists" />
+            <div className="Contents__Column block-space" />
+            <DrawSection />
+          </Contents>
+          <Test/>
+        </Wrapper>
+      </SpritesContext.Provider>
+    </WorkspaceContext.Provider>
+  );
+};
+
+const Wrapper = styled.div`
+  background: ${props => props.theme.projectBgColor};
+  width : 100vw;
+  height: 80vh;
+  overflow: x;
+`;
+
+const Contents = styled.div`
+  display: flex;
+  .block-types {
+    display: flex;
+    flex-direction: column;
+    padding: 0px 5px;
+    /* width: ; */
+    background-color: white;
+    border-top: 1px solid ${props => props.theme.mainBorderColor};
+    border-bottom: 1px solid ${props => props.theme.mainBorderColor};
+    height: 86vh;
+  }
+  .block-lists {
+    display:flex;
+    flex-direction:row-reverse;
+    min-width: 300px;
+    overflow:hidden;
+    padding:10px;
+    border: 1px solid ${props => props.theme.mainBorderColor};
+    background-color: ${props => props.theme.lightGreyColor};
+    height:86vh;
+  }
+  .block-space {
+    min-width: 500px;
+    border: 1px solid ${props => props.theme.mainBorderColor};
+    border-left: none;
+    border-radius: 0px 5px 5px 0px;
+    margin-right: 10px;
+    background-color: white;
+    width: 48vw;
+    height : 86vh;
+  }
+
+  .controller {
+    display: flex;
+    align-items: center;
+    height: 50px;
+    margin-top: -50px;
+    .play-button {
+      color: ${props => props.theme.operatorsColor};
+      &:hover {
+        color: ${props => props.theme.motionColor}
+      }
+    }
+    .stop-button {
+      margin-left: 15px;
+      color: grey;
+      &:hover {
+        color : red;
+      }
+    }
+  }
+  .setting {
+    padding: 20px;
+    margin-top: 5px;
+  }
+  .setting__row {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 10px;
+    div {
+      width: 50px;
+      text-align: center;
+    }
+    input {
+      width: 60px;
+      padding: 5px;
+      border-radius: 15px;
+      text-align: center;
+      border: 1px solid black;
+    }
+  }
+`;
+
+const TypesButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 50px;
+  div {
+    margin-top: 6px;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background-color: ${props => props.theme[`${props.btype}Color`]};
+  }
+  span {
+    margin-top: 4px;
+    font-size: 10px;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+  &:hover span {
+    color: ${props => props.theme.typeHoverColor};
+  }
+`;
+
+export default Project;
