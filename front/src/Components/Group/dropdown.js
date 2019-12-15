0a6a6634b72ee3fe12f7dbc55612dd1b8d0974df
NBJ1995 <darkmagic112@naver.com>
2019-12-15 12:52:19 +0900

    리팩토링: 키와 키의 값을 util로 분리

--- front/src/Components/Group/dropdown.js
+++ front/src/Components/Group/dropdown.js
@@ -1,20 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import PropType from 'prop-types';
 import { Select, MenuItem } from '@material-ui/core';
+import dropdownItems from '../../utils/key';
+import CONSTANTS from '../Block/constants';
 
 const Dropdown = ({ block, index }) => {
-  const [value, setValue] = useState(block.inputElement[index].value);
-  const items = ['↑', '↓', '←', '→', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  const selectRef = useRef();
+  const [value, setValue] = useState(CONSTANTS.DROPDOWN_INIT_VALUE);
   return (
     <foreignObject style={{ width: block.inputWidth[index] }}>
       <Select
-        ref={selectRef}
         value={value}
         // eslint-disable-next-line react/jsx-no-bind
         onChange={block.changeDropdownWidth(setValue).bind(block)}
+        MenuProps={{ PaperProps: { style: { maxHeight: 300 } } }}
       >
-        {items.map((item, idx) => <MenuItem value={idx} key={item}>{item}</MenuItem>)}
+        {dropdownItems.map(item => (
+          <MenuItem
+            value={Object.values(item)[0]}
+            key={Object.keys(item)[0]}
+          >
+            {Object.keys(item)[0]}
+          </MenuItem>
+        ))}
       </Select>
     </foreignObject>
   );
