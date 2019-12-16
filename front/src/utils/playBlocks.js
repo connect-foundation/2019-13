ac0df8738752eda1130e949c0ff17bad37dc2dd6
munsulim <lms1427@gmail.com>
2019-12-16 20:10:44 +0900

    변경: 블록실행을 하나의 워크스페이스가 아닌 다수의 스페이스에서 실행
     블록실행을 하나의 워크스페이스가 아닌 다수의 스페이스에서 실행하도록 변경함.

--- front/src/utils/playBlocks.js
+++ front/src/utils/playBlocks.js
@@ -1,30 +1,36 @@
 import Generator from '../Components/Block/generator';
+import WorkspaceList from '../Components/Block/workspaceList';
+
 
 let interval;
 let isPlay = false;
 const generator = new Generator();
-
 export const getIsPlay = () => isPlay;
+const workspacelist = WorkspaceList.workspaces;
 
-export const start = (workspace, isStartMode) => {
+
+export const start = (isStartMode) => {
   if (isStartMode && isPlay) {
     clearInterval(interval);
-    workspace.resetKey();
+    WorkspaceList.resetKey();
     isPlay = false;
   }
-  const startCodes = isStartMode
-    ? generator.workspaceToCode(workspace.getStartBlocks())
-    : undefined;
   isPlay = true;
-  const eventCodes = Object.entries(workspace.getEventBlocks())
+
+  const startCodes = isStartMode
+    ? workspacelist.reduce((pre, ws) => [...pre, ...generator.workspaceToCode(ws.getStartBlocks(ws),
+      false, ws.imageId)], [])
+    : undefined;
+
+  const eventCodes = workspacelist.map(ws => Object.entries(ws.getEventBlocks())
     .reduce((pre, [key, value]) => {
-      let data = value.map(block => generator.workspaceToCode(block, true));
+      let data = value.map(block => generator.workspaceToCode(block, true, ws.imageId));
       const preData = pre.get(key);
       if (preData) {
         data = [...preData, ...data];
       }
       return pre.set(key, data);
-    }, new Map());
+    }, new Map()));
   interval = setInterval(() => {
     let isEnd = true;
     if (startCodes) {
@@ -35,21 +41,22 @@ export const start = (workspace, isStartMode) => {
         }
       });
     }
-
-    eventCodes.forEach((value, key) => {
-      if (workspace.keyDown[key]) {
-        value.forEach((code) => {
-          const res = code.func.next();
-          if (res) {
-            if (res.value < code.limit) {
-              isEnd = false;
-            } else {
-              workspace.setKeyDown(key, false);
+    if (eventCodes) {
+      eventCodes.forEach(ws => ws.forEach((value, key) => {
+        if (WorkspaceList.keyDown[key]) {
+          value.forEach((code) => {
+            const res = code.func.next();
+            if (res) {
+              if (res.value < code.limit) {
+                isEnd = false;
+              } else {
+                WorkspaceList.setKeyDown(key, false);
+              }
             }
-          }
-        });
-      }
-    });
+          });
+        }
+      }));
+    }
     if (isEnd) {
       clearInterval(interval);
       isPlay = false;
