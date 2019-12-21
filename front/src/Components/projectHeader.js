e1244fcbe5a60ddb30c6a32acc777a8f8bce4827
NBJ1995 <darkmagic112@naver.com>
2019-12-21 17:07:16 +0900

    버그: 프로젝트 불러오기 시, defaultImage의 key와 값이 남아있는 버그 수정

--- front/src/Components/projectHeader.js
+++ front/src/Components/projectHeader.js
@@ -73,7 +73,7 @@ const ProjectHeader = ({ props, setReady }) => {
           const render = workspaceList.workspaces[0].setRender;
           workspaceList.workspaces = [];
           workspaceList.images = [];
-          workspaceList.dropdownItems.sprite = CONSTANTS.DROPDOWN_SPRITE_INIT_OBJECT;
+          workspaceList.dropdownItems.sprite = { wall: '벽' };
           projectData.workspaces.forEach((ws) => {
             const newWorkSpace = new Workspace({ setRender: render, id: ws.id, imageId: ws.images[0].id });
             makeBlock(ws.blocks, newWorkSpace);
